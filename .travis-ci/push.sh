76659973fb95df6e84d19600b863f62f0ad1abaa
Scott Ouellette <scottx611x@gmail.com>
2018-10-25 17:46:08 -0400

    no checkout and no quiet pushing

--- .travis-ci/push.sh
+++ .travis-ci/push.sh
@@ -6,14 +6,13 @@ setup_git() {
 }
 
 commit_coverage_files() {
-  git checkout -b master
   git add coverage
   git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
 }
 
 upload_files() {
   git remote add origin-master https://${GH_TOKEN}@github.com/scottx611x/problem-solving-with-algorithms-and-data-structures-using-python.git > /dev/null 2>&1
-  git push --quiet --set-upstream origin-master master 
+  git push --set-upstream origin-master master 
 }
 
 setup_git
