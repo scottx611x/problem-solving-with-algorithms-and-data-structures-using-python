465120bd1e6c84fc36fb89407b0e39cc08efa5d1
Scott Ouellette <scottx611x@gmail.com>
2018-10-25 17:25:24 -0400

    Auto commit coverage reports from travis on master builds

---
+++ .travis-ci/push.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+setup_git() {
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "Travis CI"
+}
+
+commit_coverage_files() {
+  git checkout -b master
+  git add htmlcov
+  git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
+}
+
+upload_files() {
+  git remote add origin-master https://${GH_TOKEN}@github.com/scottx611x/problem-solving-with-algorithms-and-data-structures-using-python
+.git > /dev/null 2>&1
+  git push --quiet --set-upstream origin-master master 
+}
+
+setup_git
+commit_coverage_files
+upload_files
