09663091ae5d3256759d22685ecf2b153c5614b7
Scott Ouellette <scottx611x@gmail.com>
2018-10-26 10:20:12 -0400

    Specify origin-master

--- .travis-ci/push.sh
+++ .travis-ci/push.sh
@@ -4,8 +4,7 @@ setup_git() {
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "Travis CI"
   git remote rm origin
-  git remote add origin https://scottx611x:${GH_TOKEN}@github.com/scottx611x/problem-solving-with-algorithms-and-data-structures-using-python.git
-
+  git remote add origin-master https://${GH_TOKEN}@github.com/scottx611x/problem-solving-with-algorithms-and-data-structures-using-python.git
 }
 
 commit_coverage_files() {
