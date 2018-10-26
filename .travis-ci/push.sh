1bcd93c821f76c32804ddaa276e0f162be6d20f3
Scott Ouellette <scottx611x@gmail.com>
2018-10-26 10:16:38 -0400

    Specify remote a bit differently

--- .travis-ci/push.sh
+++ .travis-ci/push.sh
@@ -3,6 +3,9 @@
 setup_git() {
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "Travis CI"
+  git remote rm origin
+  git remote add origin https://scottx611x:${GH_TOKEN}@github.com/scottx611x/problem-solving-with-algorithms-and-data-structures-using-python.git
+
 }
 
 commit_coverage_files() {
@@ -11,7 +14,6 @@ commit_coverage_files() {
 }
 
 upload_files() {
-  git remote add origin-master https://${GH_TOKEN}@github.com/scottx611x/problem-solving-with-algorithms-and-data-structures-using-python.git > /dev/null 2>&1
   git push --force --set-upstream origin-master master 
 }
 
