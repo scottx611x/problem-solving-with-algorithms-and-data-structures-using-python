f1513099eb49b3de3b69004e66dd6760eb87720a
Scott Ouellette <scottx611x@gmail.com>
2018-11-20 17:08:12 -0500

    Add sudoku and tests

--- section1/sudoku.py
+++ section1/sudoku.py
@@ -1,103 +1,168 @@
+from pprint import pprint
+
 
 class SudokuGrid:
-    BLANK = "X"
 
     def __init__(self):
         self.grid = [
-            [
-                [6, self.BLANK, 7],
-                [self.BLANK, 1, self.BLANK],
-                [self.BLANK, 9, self.BLANK]
-            ],
-            [
-                [4, 1, self.BLANK],
-                [3, 9, self.BLANK],
-                [6, 2, self.BLANK]
-            ],
-            [
-                [self.BLANK, 9, self.BLANK],
-                [6, self.BLANK, self.BLANK],
-                [self.BLANK, self.BLANK, 5]
-            ],
-            [
-                [2, self.BLANK, self.BLANK],
-                [7, self.BLANK, self.BLANK],
-                [self.BLANK, 8, 3]
-            ],
-            [
-                [7, 5, 1],
-                [8, 3, 9],
-                [2, self.BLANK, self.BLANK]
-            ],
-            [
-                [self.BLANK, 8, 3],
-                [self.BLANK, 6, 4],
-                [self.BLANK, 5, 1]
-            ],
-            [
-                [1, 7, self.BLANK],
-                [self.BLANK, 4, self.BLANK],
-                [8, 3, 9]
-            ],
-            [
-                [5, 4, 2],
-                [9, 8, self.BLANK],
-                [self.BLANK, 7, 6]
-            ],
-            [
-                [self.BLANK, 3, 9],
-                [1, 7, self.BLANK],
-                [5, 4, self.BLANK]
-            ]
+            [6, 0, 7, 4, 1, 0, 0, 9, 0],
+            [0, 1, 0, 3, 9, 0, 6, 0, 0],
+            [0, 9, 0, 6, 2, 0, 0, 0, 5],
+            [2, 0, 0, 7, 5, 1, 0, 8, 3],
+            [7, 0, 0, 8, 3, 9, 0, 6, 4],
+            [0, 8, 3, 2, 0, 0, 0, 5, 1],
+            [1, 7, 0, 5, 4, 2, 0, 3, 9],
+            [0, 4, 0, 9, 8, 0, 1, 7, 0],
+            [8, 3, 9, 0, 7, 6, 5, 4, 0]
         ]
 
-    def get_rows(self):
-        rows = [[], [], [], [], [], [], [], [], []]
-        offset = 0
-        count = 0
+    def update_grid(self, coords, value):
+        x, y = coords
+        self.grid[x][y] = value
+        return self.grid
 
-        for i in range(len(self.grid)):
-            for index, row in enumerate(self.grid[i]):
-                rows[index + offset].extend(row)
-                count += 1
-                if count == 9:
-                    count = 0
-                    offset += 3
-        return rows
+    def display(self):
+        pprint(self.grid)
+
+    def _flatten(self):
+        return sum(self.grid, [])
+
+    @property
+    def is_full(self):
+        return not any([i == 0 for i in self._flatten()])
 
     def get_columns(self):
         columns = [[], [], [], [], [], [], [], [], []]
         for i in range(len(self.grid)):
-            for row in self.get_rows():
+            for row in self.grid:
                 columns[i].append(row[i])
         return columns
 
-    def get_nonets(self):
-        return [sum(nonets, []) for nonets in self.grid[0:9]]
+    def get_boxes(self):
+        rows_a = self.grid[:3]
+        rows_b = self.grid[3:6]
+        rows_c = self.grid[6:9]
+
+        boxes = [[]] * 9
+        boxes[0] = rows_a[0][:3] + rows_a[1][:3] + rows_a[2][:3]
+        boxes[1] = rows_a[0][3:6] + rows_a[1][3:6] + rows_a[2][3:6]
+        boxes[2] = rows_a[0][6:9] + rows_a[1][6:9] + rows_a[2][6:9]
+        boxes[3] = rows_b[0][:3] + rows_b[1][:3] + rows_b[2][:3]
+        boxes[4] = rows_b[0][3:6] + rows_b[1][3:6] + rows_b[2][3:6]
+        boxes[5] = rows_b[0][6:9] + rows_b[1][6:9] + rows_b[2][6:9]
+        boxes[6] = rows_c[0][:3] + rows_c[1][:3] + rows_c[2][:3]
+        boxes[7] = rows_c[0][3:6] + rows_c[1][3:6] + rows_c[2][3:6]
+        boxes[8] = rows_c[0][6:9] + rows_c[1][6:9] + rows_c[2][6:9]
+
+        return boxes
+
+    def _get_coordinate_list(self):
+        grid_length = len(self.grid)
+        r = range(grid_length)
+        a = [i for i in range(3)] * grid_length
+        b = [i for i in range(3, 6)] * grid_length
+        c = [i for i in range(6, grid_length)] * grid_length
+        x_coords = a + b + c
+        y_coords = sorted(list(r) * 3) * 3
+
+        def get_chunks(l, chunk_size=grid_length):
+            """Yield successive n-sized chunks from a list."""
+            for i in range(0, len(l), chunk_size):
+                yield l[i:i + chunk_size]
+
+        return list(get_chunks(list(zip(x_coords, y_coords))))
+
+    def get_box_from_coords(self, coords):
+        box_index_to_coords_map = {
+            i: coord_list
+            for i, coord_list in enumerate(self._get_coordinate_list())
+        }
+
+        for box_index, coords_in_box in box_index_to_coords_map.items():
+            if coords in coords_in_box:
+                return self.get_boxes()[box_index]
+
+    def get_peers(self, coords):
+        i, j = coords
+        return set(
+            self.grid[i] + self.get_columns()[j] +
+            self.get_box_from_coords(coords)
+        )
+
+    def get_grid_value(self, coords):
+        i, j = coords
+        return self.grid[i][j]
+
+    def get_possible_entries(self, coords):
+        value = self.get_grid_value(coords)
+
+        if not value == 0:
+            return []
+
+        possible_entries = list(range(1, 10))
+
+        for square in self.get_peers(coords):
+            if square in possible_entries:
+                possible_entries.remove(square)
+
+        return possible_entries
+
+    def get_square_with_least_possibilities(self):
+        least_possible_entries = None
+        coords = None
+        for x in range(9):
+            for y in range(9):
+                possible_entries = self.get_possible_entries((x, y))
+                if possible_entries:
+                    if least_possible_entries is None:
+                        coords = (x, y)
+                        least_possible_entries = possible_entries
+                    else:
+                        if len(possible_entries) < len(least_possible_entries):
+                            coords = (x, y)
+                            least_possible_entries = possible_entries
+        return coords, least_possible_entries
 
 
 class SudokuSolver(SudokuGrid):
-    def solve(self):
-        pass
 
-    def _check_okay(self, rows_columns_or_nonets):
-        for cleaned in [
-            [i for i in j if isinstance(i, int)]
-            for j in rows_columns_or_nonets
-        ]:
-            if sum(cleaned) != 9:
+    def solve(self):
+        if self.is_solved():
+            print("Sudoku Solved!")
+            self.display()
+            return self.grid
+
+        coords, possible_entries = self.get_square_with_least_possibilities()
+        x, y = coords
+        if len(possible_entries) == 1:
+            self.update_grid((x, y), possible_entries[0])
+            return self.solve()
+        else:
+            for possible_entry in possible_entries:
+                self.update_grid((x, y), possible_entry)
+                return self.solve()
+            self.update_grid((x, y), 0)
+
+    def _check_okay(self, rows_columns_or_boxes):
+        for i in rows_columns_or_boxes:
+            if sum(i) != 45:
                 return False
         else:
             return True
 
     @property
     def rows_okay(self):
-        return self._check_okay(self.get_rows())
+        return self._check_okay(self.grid)
 
     @property
     def columns_okay(self):
         return self._check_okay(self.get_columns())
 
     @property
-    def nonets_okay(self):
-        return self._check_okay(self.get_nonets())
+    def boxes_okay(self):
+        return self._check_okay(self.get_boxes())
+
+    def is_solved(self):
+        return all(
+            [self.is_full, self.rows_okay, self.columns_okay, self.boxes_okay]
+        )
--- section1/test_sudoku.py
+++ section1/test_sudoku.py
@@ -1,6 +1,7 @@
 from unittest import TestCase
 
 from section1.sudoku import SudokuGrid, SudokuSolver
+from section1.utils import captured_output
 
 
 class SudokuGridTests(TestCase):
@@ -11,49 +12,119 @@ class SudokuGridTests(TestCase):
         self.assertEqual(
             self.sudoku_grid.get_columns(),
             [
-                [6, 'X', 'X', 2, 7, 'X', 1, 'X', 8],
-                ['X', 1, 9, 'X', 'X', 8, 7, 4, 3],
-                [7, 'X', 'X', 'X', 'X', 3, 'X', 'X', 9],
-                [4, 3, 6, 7, 8, 2, 5, 9, 'X'],
-                [1, 9, 2, 5, 3, 'X', 4, 8, 7],
-                ['X', 'X', 'X', 1, 9, 'X', 2, 'X', 6],
-                ['X', 6, 'X', 'X', 'X', 'X', 'X', 1, 5],
-                [9, 'X', 'X', 8, 6, 5, 3, 7, 4],
-                ['X', 'X', 5, 3, 4, 1, 9, 'X', 'X']
+                [6, 0, 0, 2, 7, 0, 1, 0, 8],
+                [0, 1, 9, 0, 0, 8, 7, 4, 3],
+                [7, 0, 0, 0, 0, 3, 0, 0, 9],
+                [4, 3, 6, 7, 8, 2, 5, 9, 0],
+                [1, 9, 2, 5, 3, 0, 4, 8, 7],
+                [0, 0, 0, 1, 9, 0, 2, 0, 6],
+                [0, 6, 0, 0, 0, 0, 0, 1, 5],
+                [9, 0, 0, 8, 6, 5, 3, 7, 4],
+                [0, 0, 5, 3, 4, 1, 9, 0, 0]
             ]
         )
 
-    def test_get_rows(self):
+    def test_get_grid(self):
         self.assertEqual(
-            self.sudoku_grid.get_rows(),
+            self.sudoku_grid.grid,
             [
-                [6, 'X', 7, 4, 1, 'X', 'X', 9, 'X'],
-                ['X', 1, 'X', 3, 9, 'X', 6, 'X', 'X'],
-                ['X', 9, 'X', 6, 2, 'X', 'X', 'X', 5],
-                [2, 'X', 'X', 7, 5, 1, 'X', 8, 3],
-                [7, 'X', 'X', 8, 3, 9, 'X', 6, 4],
-                ['X', 8, 3, 2, 'X', 'X', 'X', 5, 1],
-                [1, 7, 'X', 5, 4, 2, 'X', 3, 9],
-                ['X', 4, 'X', 9, 8, 'X', 1, 7, 'X'],
-                [8, 3, 9, 'X', 7, 6, 5, 4, 'X']
+                [6, 0, 7, 4, 1, 0, 0, 9, 0],
+                [0, 1, 0, 3, 9, 0, 6, 0, 0],
+                [0, 9, 0, 6, 2, 0, 0, 0, 5],
+                [2, 0, 0, 7, 5, 1, 0, 8, 3],
+                [7, 0, 0, 8, 3, 9, 0, 6, 4],
+                [0, 8, 3, 2, 0, 0, 0, 5, 1],
+                [1, 7, 0, 5, 4, 2, 0, 3, 9],
+                [0, 4, 0, 9, 8, 0, 1, 7, 0],
+                [8, 3, 9, 0, 7, 6, 5, 4, 0]
             ]
         )
 
-    def test_get_nonets(self):
+    def test_get_boxes(self):
         self.assertEqual(
-            self.sudoku_grid.get_nonets(),
+            self.sudoku_grid.get_boxes(),
             [
-                [6, 'X', 7, 'X', 1, 'X', 'X', 9, 'X'],
-                [4, 1, 'X', 3, 9, 'X', 6, 2, 'X'],
-                ['X', 9, 'X', 6, 'X', 'X', 'X', 'X', 5],
-                [2, 'X', 'X', 7, 'X', 'X', 'X', 8, 3],
-                [7, 5, 1, 8, 3, 9, 2, 'X', 'X'],
-                ['X', 8, 3, 'X', 6, 4, 'X', 5, 1],
-                [1, 7, 'X', 'X', 4, 'X', 8, 3, 9],
-                [5, 4, 2, 9, 8, 'X', 'X', 7, 6],
-                ['X', 3, 9, 1, 7, 'X', 5, 4, 'X']
+                [6, 0, 7, 0, 1, 0, 0, 9, 0],
+                [4, 1, 0, 3, 9, 0, 6, 2, 0],
+                [0, 9, 0, 6, 0, 0, 0, 0, 5],
+                [2, 0, 0, 7, 0, 0, 0, 8, 3],
+                [7, 5, 1, 8, 3, 9, 2, 0, 0],
+                [0, 8, 3, 0, 6, 4, 0, 5, 1],
+                [1, 7, 0, 0, 4, 0, 8, 3, 9],
+                [5, 4, 2, 9, 8, 0, 0, 7, 6],
+                [0, 3, 9, 1, 7, 0, 5, 4, 0]
             ]
         )
+
+    def test_get_box_from_coords(self):
+        self.assertEqual(
+            self.sudoku_grid.get_box_from_coords((1, 1)),
+            self.sudoku_grid.get_boxes()[0]
+        )
+        self.assertEqual(
+            self.sudoku_grid.get_box_from_coords((1, 4)),
+            self.sudoku_grid.get_boxes()[1]
+        )
+        self.assertEqual(
+            self.sudoku_grid.get_box_from_coords((1, 7)),
+            self.sudoku_grid.get_boxes()[2]
+        )
+        self.assertEqual(
+            self.sudoku_grid.get_box_from_coords((4, 1)),
+            self.sudoku_grid.get_boxes()[3]
+        )
+        self.assertEqual(
+            self.sudoku_grid.get_box_from_coords((4, 4)),
+            self.sudoku_grid.get_boxes()[4]
+        )
+        self.assertEqual(
+            self.sudoku_grid.get_box_from_coords((4, 7)),
+            self.sudoku_grid.get_boxes()[5]
+        )
+        self.assertEqual(
+            self.sudoku_grid.get_box_from_coords((7, 1)),
+            self.sudoku_grid.get_boxes()[6]
+        )
+        self.assertEqual(
+            self.sudoku_grid.get_box_from_coords((7, 4)),
+            self.sudoku_grid.get_boxes()[7]
+        )
+        self.assertEqual(
+            self.sudoku_grid.get_box_from_coords((7, 7)),
+            self.sudoku_grid.get_boxes()[8]
+        )
+
+    def test_is_full(self):
+        self.assertFalse(self.sudoku_grid.is_full)
+
+    def test_get_possible_entries(self):
+        self.assertEqual(
+            self.sudoku_grid.get_possible_entries((0, 1)),
+            [2, 5]
+        )
+        self.assertEqual(
+            self.sudoku_grid.get_possible_entries((3, 1)),
+            [6]
+        )
+        self.assertEqual(
+            self.sudoku_grid.get_possible_entries((0, 8)),
+            [2, 8]
+        )
+
+    def test_display_grid(self):
+        with captured_output() as output:
+            self.sudoku_grid.display()
+        self.assertEqual("""[[6, 0, 7, 4, 1, 0, 0, 9, 0],
+ [0, 1, 0, 3, 9, 0, 6, 0, 0],
+ [0, 9, 0, 6, 2, 0, 0, 0, 5],
+ [2, 0, 0, 7, 5, 1, 0, 8, 3],
+ [7, 0, 0, 8, 3, 9, 0, 6, 4],
+ [0, 8, 3, 2, 0, 0, 0, 5, 1],
+ [1, 7, 0, 5, 4, 2, 0, 3, 9],
+ [0, 4, 0, 9, 8, 0, 1, 7, 0],
+ [8, 3, 9, 0, 7, 6, 5, 4, 0]]
+""", output.getvalue())
+
 
 class SudokuSolverTests(TestCase):
     def setUp(self):
@@ -65,12 +136,20 @@ class SudokuSolverTests(TestCase):
     def test_columns_okay(self):
         self.assertFalse(self.sudoku_solver.columns_okay)
 
-    def test_nonets_okay(self):
-        self.assertFalse(self.sudoku_solver.nonets_okay)
+    def test_boxes_okay(self):
+        self.assertFalse(self.sudoku_solver.boxes_okay)
 
     def test_solve(self):
-        self.assertEqual(
-            self.sudoku_solver.solve(),
-            "IMPLEMENT THE EXACT COVER/ DANCING LINKS ALGORITHM"
-            "https://en.wikipedia.org/wiki/Exact_cover#Detailed_example"
-        )
+        with captured_output() as output:
+            self.sudoku_solver.solve()
+        self.assertEqual("""Sudoku Solved!
+[[6, 2, 7, 4, 1, 5, 3, 9, 8],
+ [4, 1, 5, 3, 9, 8, 6, 2, 7],
+ [3, 9, 8, 6, 2, 7, 4, 1, 5],
+ [2, 6, 4, 7, 5, 1, 9, 8, 3],
+ [7, 5, 1, 8, 3, 9, 2, 6, 4],
+ [9, 8, 3, 2, 6, 4, 7, 5, 1],
+ [1, 7, 6, 5, 4, 2, 8, 3, 9],
+ [5, 4, 2, 9, 8, 3, 1, 7, 6],
+ [8, 3, 9, 1, 7, 6, 5, 4, 2]]
+""", output.getvalue())
